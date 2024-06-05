46020a5558251a957d929be1e5956c275f8c1f73
Fadwa Talik <fadwa.bayoudh@ecoles-epsi.net>
2024-06-05 10:19:52 +0200

    📦 NEW: Le test de fichier math est vérifié

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+from typing import Union
+
 from fastapi import FastAPI
-from app.api.v1.user import router
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"status" : "ok"}
+    return {"Hello": "World"}
 
-app.include_router(router)
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
